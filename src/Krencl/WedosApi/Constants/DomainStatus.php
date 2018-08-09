827043c003fb70c42fff9d27bac0344e99217b95
Krencl <krencl@gmail.com>
2018-08-09 18:59:16 +0000

    domain statuses

---
+++ src/Krencl/WedosApi/Constants/DomainStatus.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Krencl\WedosApi\Constants;
+
+class DomainStatus
+{
+	const ACTIVE = 'active';
+	const CANCELED = 'canceled';
+	const DELETED = 'deleted';
+	const DISABLED = 'disabled';
+	const EXPIRED = 'expired';
+	const FEE_WAITING = 'fee_waiting';
+	const RENEW_PROCESSING = 'renew_processing';
+	const SETUP_PROCESSING = 'setup_processing';
+	const TRANSFER_FEE_WAITING = 'transfer_fee_waiting';
+	const TRANSFER_PROCESSING = 'transfer_processing';
+}
