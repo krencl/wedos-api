9691aa0fc597f61cb7623ab7892543f97d2810b1
Krencl <krencl@gmail.com>
2018-08-11 14:19:24 +0200

    fixed DomainsListTest

--- tests/DomainsListTest.php
+++ tests/DomainsListTest.php
@@ -10,10 +10,7 @@ final class DomainsListTest extends TestCase
 		$configuration = WedosApi\Configuration::createFromFile(__DIR__ . '/../config.json');
 		$client = new WedosApi\Client($configuration);
 
-//		$command = new Krencl\WedosApi\Command\DnsRowAdd('krencl.cz','www', WedosApi\Constants\DNSType::TXT, 'test');
-//		$command = new Krencl\WedosApi\Command\DnsRowUpdate('krencl.cz', 48262, 'test update',3600);
-//		$command = new Krencl\WedosApi\Command\DnsRowsList('krencl.cz');
-		$command = new Krencl\WedosApi\Command\DnsRowDelete('krencl.cz', 48262);
+		$command = new Krencl\WedosApi\Command\DomainsList(Krencl\WedosApi\Constants\DomainStatus::ACTIVE);
 		$response = $client->sendRequest($command);
 
 		$this->assertInstanceOf(WedosApi\Response::class, $response);
